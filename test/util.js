c98d1c8403e7fe0b8ab87bc1aec07692c2fcc7af
vzvu3k6k <vzvu3k6k@gmail.com>
2016-11-24 00:08:03 +0900

    Add tests for extractAntennaID

---
+++ test/util.js
@@ -0,0 +1,27 @@
+let assert = require('assert')
+let util = require('../lib/util')
+
+describe('extractAntennaID', () => {
+  let validTestCases = {
+    'a series of integer': '123456789012345678',
+    'an antenna permalink': 'https://daichkr.hatelabo.jp/antenna/123456789012345678',
+    'a collection permalink': 'https://daichkr.hatelabo.jp/collection/123456789012345678',
+    'a permalink without scheme': 'daichkr.hatelabo.jp/antenna/123456789012345678',
+    'a permalink with trailing spaces': '   https://daichkr.hatelabo.jp/collection/123456789012345678'
+  }
+  for (let [type, arg] of Object.entries(validTestCases)) {
+    it(`should return an antenna ID when the argument is ${type}`, () => {
+      assert.strictEqual(util.extractAntennaID(arg), '123456789012345678')
+    })
+  }
+
+  it('should return null when the argument is invalid', () => {
+    let testCases = [
+      '',
+      'foo'
+    ]
+    for (let arg of testCases) {
+      assert.strictEqual(util.extractAntennaID(arg), null)
+    }
+  })
+})
